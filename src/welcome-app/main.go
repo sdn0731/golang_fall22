ce64ae6d02e4bd417ddb7701e5c1ddc4c1a9ee93
Sarah "Dasha" Nolan <snolan0731@gmail.com>
2023-04-25 18:18:53 +0000

    created contact structs

--- src/welcome-app/main.go
+++ src/welcome-app/main.go
@@ -8,59 +8,89 @@ import (
 	"encoding/json"
 )
 
-// Create a struct that holds information to be displayed in our HTML file
 type Welcome struct {
 	Name string
 	Time string
 }
 
+// Create JsonResponse Struct
 type JsonResponse struct {
 	Value1 string `json:"key1"`
 	Value2 string `json:"key2"`
+	JsonNested JsonNested `json:"jsonNested"`
 }
 
+// Create JsonNested Struct
+type JsonNested struct {
+	NestedValue1 string `json:"nestedKey1"`
+	NestedValue2 string `json:"nestedKey2"`
+}
 
-// Go application entrypoint
+// Create Contact Struct
+type Contact struct {
+	FirstName string `json:"firstName"`
+	LastName string `json:"lastName"`
+	Address Address `json:"address"`
+	ContactInfo ContactInfo `json:"contactInfo"`
+}
+
+// Create Nested Address Struct
+type Address struct {
+	Street string `json:"street"`
+	City string `json:"city"`
+}
+
+// Create Nested ContactInfo Struct
+type ContactInfo struct {
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
+
 func main() {
-	//Instantiate a Welcome struct object and pass in some random information.
-	//We shall get the name of the user as a query parameter from the URL
 	welcome := Welcome{"Anonymous", time.Now().Format(time.Stamp)}
-
-	//We tell Go exactly where we can find our html file. We ask Go to parse the html file (Notice
-	// the relative path). We wrap it in a call to template.Must() which handles any errors and halts if there are fatal errors
-
 	templates := template.Must(template.ParseFiles("templates/welcome-template.html"))
 
-	// Create Json Response
+	// Create Json Nested Object
+	nested := JsonNested{
+		NestedValue1: "first nested value",
+		NestedValue2: "second nested value",
+	}
+
+	// Create Json Response Object
 	jsonResp := JsonResponse{
 		Value1: "some Data",
 		Value2: "other Data",
+		JsonNested: nested,
 	}
 
-	// Test Print
-	fmt.Fprint("%+v\n\n\n\n", jsonResp);
+	// Create ContactInfo Nested Object
+	nestedContact := ContactInfo{
+		Email: "taylorswift@umgstores.com",
+		Phone: "2301301978",
+	}
 
-	//Our HTML comes with CSS that go needs to provide when we run the app. Here we tell go to create
-	// a handle that looks in the static directory, go then uses the "/static/" as a url that our
-	//html can refer to when looking for our css and other files.
+	// Create Address Nested Object
+	nestedAddress := Address{
+		Street: "23 Cornelia Street",
+		City: "New York, New York",
+	}
 
-	http.Handle("/static/", //final url can be anything
+	// Create Contact Response Object
+	contactResp := Contact{
+		FirstName: "Taylor",
+		LastName: "Swift",
+		Address: nestedAddress,
+		ContactInfo: nestedContact,
+	}
+
+	http.Handle("/static/",
 		http.StripPrefix("/static/",
-			http.FileServer(http.Dir("static")))) //Go looks in the relative "static" directory first using http.FileServer(), then matches it to a
-	//url of our choice as shown in http.Handle("/static/"). This url is what we need when referencing our css files
-	//once the server begins. Our html code would therefore be <link rel="stylesheet"  href="/static/stylesheet/...">
-	//It is important to note the url in http.Handle can be whatever we like, so long as we are consistent.
+			http.FileServer(http.Dir("static"))))
 
-	//This method takes in the URL path "/" and a function that takes in a response writer, and a http request.
-	// **** THIS IS THE MAIN PATH /
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		//Takes the name from the URL query e.g ?name=Martin, will set welcome.Name = Martin.
 		if name := r.FormValue("name"); name != "" {
 			welcome.Name = name
 		}
-		//If errors show an internal server error message
-		//I also pass the welcome struct to the welcome-template.html file.
 		if err := templates.ExecuteTemplate(w, "welcome-template.html", welcome); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
@@ -68,13 +98,14 @@ func main() {
 
 	// Create jsonResponse Handler
 	http.HandleFunc("/jsonResponse", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "JSON goes here")
+		json.NewEncoder(w).Encode(jsonResp)
 	})
 
-	//Start the web server, set the port to listen to 8080. Without a path it assumes localhost
-	//Print any errors from starting the webserver using fmt
+	// Create contactResponse Handler
+	http.HandleFunc("/contactResponse", func(w http.ResponseWriter, r *http.Request) {
+		json.NewEncoder(w).Encode(contactResp)
+	})
+
 	fmt.Println("Listening")
 	fmt.Println(http.ListenAndServe(":8080", nil))
 }
-
-// Test comment
